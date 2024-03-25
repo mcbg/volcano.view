7b898f5074cc6a65d7cffe0d541d87673f339563
MZCG (Michael Carter Bisgaard Galanakis) <MZCG@novonordisk.com>
2024-03-25 15:59:13 +0100

    use + operator to add modules

--- R/volcano.view.R
+++ R/volcano.view.R
@@ -3,29 +3,47 @@ vw = function(..., type) {
   structure(list(...), class = c('vw', 'list'), type = type)
 }
 
-volcano.view = function(data, ...) {
+#' @importFrom jsonlite unbox
+#' @export
+volcano.view = function(data, x, y, id, ...) {
   res = vw(
-    view = jsonlite::unbox('volcano'),
+    view = unbox('volcano'),
     payload = list(
       data = as.list(data),
-      config = list(...) |> lapply(jsonlite::unbox)
+      config = list(x = x, y = y, name = id) |> lapply(unbox)
     ),
     type = 'main'
   )
 }
 
+#' @export
 module.drill = function(...) {
   vw(drill = list(...), type = 'module')
 }
 
+#' @export
 module.details = function(...) {
   vw(details = list(...), type = 'module')
 }
 
-save = function(filename) {
-
+#' @export
+module.drillplots = function(cols) {
+  vw(`drill-plots`= cols, type = 'module')
 }
 
+#' @export
+module.enrichment = function(data, drill=c()) {
+  vw(enrichment=list(data=data, drill = drill), type = 'module')
+}
+
+#' @param collection A list where each element is a vector of genes. The elements should be named.
+#' @example module.genelist(list(housekeeping = c('ALBUMIN', 'GAPDH'), special = c('VHL', 'IL6R')))
+#' @export
+module.genelist = function(collection) {
+  vw(`gene-list`=list(genelist=genelists), type = 'module')
+}
+
+#' @export
 send = \(x) {
   o = x |> .convert()
   httr::POST('http://127.0.0.1:8999/api/setState', body = jsonlite::toJSON(o), encode = 'raw')
@@ -40,27 +58,12 @@ send = \(x) {
 }
 
 
-aa = x |> convert()
-
-library(jsonlite)
-list(
-  view = unbox('volcano'),
-  payload=list(
-    useCanvas = TRUE,
-    data = ds |> as.list(),
-    config=list(x='mpg', y='disp', label='rn') |> lapply(unbox),
-    modules=list(
-      drill = c('gear')
-    )
-  )
-) |>
-  send()
-
-
-ds = mtcars |> data.table(keep.rownames = TRUE)
-list(
-  volcano.view(ds, x='mpg', y='disp', label = 'rn'),
-  module.drill('mpg', 'disp'),
-  module.details('gear', 'disp')
-) |> send()
-
+#' @export
+"+.vw" = \(x,y) {
+  if ('vw-collect' %in% class(x)) {
+    c(x, list(y))
+  }
+  else {
+    structure(list(x, y), class = c('vw-collect', 'vw', 'list'))
+  }
+}
