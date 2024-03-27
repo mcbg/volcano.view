3339fde079fa0f446643e8710cc1492bb6e98874
mcbg <26263635+mcbg@users.noreply.github.com>
2024-03-27 21:40:58 +0100

    modules moved

--- R/volcano.view.R
+++ R/volcano.view.R
@@ -18,44 +18,6 @@ volcano.view = function(data, x, y, id, ...) {
 }
 
 #' @export
-module.drill = function(...) {
-
-  vw(drill = c(...), type = 'module')
-}
-
-#' @export
-module.details = function(...) {
-  vw(details = c(...), type = 'module')
-}
-
-#' @export
-module.drillplots = function(...) {
-  vw(`drill-plots`= c(...), type = 'module')
-}
-
-#' @export
-module.enrichment = function(data, drill=c()) {
-  vw(enrichment=list(data=data, drill = drill), type = 'module')
-}
-
-#' Adds gene list module to dashboard
-#'
-#' @param collection A list where each element is a vector of genes, where the elements should be named.
-#' @examples
-#'  module.genelist(list(housekeeping = c('ALBUMIN', 'GAPDH'), special = c('VHL', 'IL6R')))
-#' @export
-module.genelist = function(collection) {
-  vw(`gene-list`=list(genelist=collection), type = 'module')
-}
-
-#' Adds confidence interval to selected point
-#'
-#' @export
-module.ci = function(low, high) {
-  vw(`confidence-interval`=list(low = unbox(low), high = unbox(high)), type = 'module')
-}
-
-#' @export
 send = \(x) {
   o = x |> .convert()
   httr::POST('http://127.0.0.1:8999/api/setState', body = jsonlite::toJSON(o), encode = 'raw')
